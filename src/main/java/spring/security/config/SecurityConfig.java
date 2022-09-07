aacb277c8c87a7a96541566a059bbc63df92dcd9
ChanGenie <ckdwls0702@naver.com>
2022-09-07 16:13:04 +0900

    cors 설정

--- src/main/java/spring/security/config/SecurityConfig.java
+++ src/main/java/spring/security/config/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
                 .and()
-                .cors()
+                .cors().configurationSource(corsConfigurationSource())
 
                 // 로그인 회원가입 API는 토큰이 없는 상태에서 요청이 들어오기 때문에 permitAll 설정
                 .and()
--- src/main/java/spring/security/dto/MemberRequestDto.java
+++ src/main/java/spring/security/dto/MemberRequestDto.java
@@ -14,6 +14,7 @@ import javax.validation.constraints.NotBlank;
 @ToString
 public class MemberRequestDto {
 
+    @NotBlank
     @Email
     private String email;
 
